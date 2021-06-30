18f0e8aab7252e708a7ea1e66d782ce6356c690f
jaymalave <jaym.github@gmail.com>
2021-06-30 21:33:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector(".menu-list");
+menu.addEventListener("mouseover", (event) => {
+  if (event.target.classList.contains("menu-link")) {
+    menu.style.setProperty(
+      "--underline-width",
+      `${event.target.offsetWidth}px`
+    );
+    menu.style.setProperty(
+      "--underline-offset-x",
+      `${event.target.offsetLeft}px`
+    );
+  }
+});
+menu.addEventListener("mouseleave", () =>
+  menu.style.setProperty("--underline-width", "0")
+);
